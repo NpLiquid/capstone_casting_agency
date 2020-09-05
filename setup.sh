d0482bb5335197b07e4fd29b9527cda97bce202d
Ari Barrera <NpLiquid@users.noreply.github.com>
2020-09-05 19:57:27 +0100

    Add files via upload
    Updating authorization tokens

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 export AUTH0_DOMAIN='fsdu.au.auth0.com'
 export ALGORITHMS=['RS256']
 export API_AUDIENCE='cau'
-export DATABASE_URL="postgres://{}:{}@{}/{}".format('postgres', 'postgres','localhost:5432', "casting_agency")
+export AUTH0_CLIENT_ID = 'oTJMgJfqwu0WBzlYPXFpeRKFA0AS50KM'
+export AUTH0_CLIENT_SECRET = 'criEASIAYHKe4tgJd9FQAbOIZo_47qrgg05GkbDwhZFRkP0vuH4JsbDbyhg-1ACl'
+
+export DATABASE_URL="postgresql://postgres:postgres@postgres@localhost:5432/casting_agency")
+
+export ENV='development'
+export FLASK_APP=app
+export FLASK_DEBUG=True
